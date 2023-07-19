d7847ddc94fb00698c3af2fab3943c7940b9daa8
shaddyslim <shaddyslim@icloud.com>
2023-07-19 13:50:05 -0700

    new file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,6 @@
--- a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256));
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2.
+ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+ GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+ FLUSH PRIVILEGES;
+ 
